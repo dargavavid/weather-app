f26929bd28bd07f3b73486575d4ed8f5e282aa57
dargavavid <dargavavid@gmail.com>
2018-01-29 08:35:19 +0100

    Configure argv

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const request = require('request');
 const yargs = require('yargs');
+
+
+const argv = yargs
+    .options({
+        address: {
+            describe: 'Address to fetch weather for',
+            demand: true,
+            alias: 'a',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
